50eef1e2ebd140ed9a89928423024256bcbc56b1
Aritra Bala <aritra07@gmail.com>
2025-06-13 00:08:12 +0530

    add initial files for backend

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+    res.send('aritra.com')
+})
+app.get('/github',(req,res)=>{
+    res.send('<h1>github.com</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
