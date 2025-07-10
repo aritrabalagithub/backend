5233cbb57dfce690a666e2dc945edda5b8e157ef
Aritra Bala <aritra07@gmail.com>
2025-07-10 13:37:44 +0530

    updated

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoosefrom  from "mongoose";
+import { DB_NAME } from "../constants";
+import mongoose from "mongoose";
+
+const connectDB=async()=>{
+    try{
+       const connectionInstance= await mongoose.connect(`${'process.env.MONGODB_URI'}/${DB_NAME}`)
+    console.log(`\n MongoDb connected!!DB HOST:${connectionInstance.connection.host}`);
+    }
+    catch(error){
+       console.log("MONGODB connect error",error) ;
+       process.exit(1);
+    }
+}
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import express from "express";
-const app=express()
+//require.apply('dotenv').config({path:'./env'})
+ import dotenv from "dotenv"
+ import connectDB from "./db";
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
 
-;(async ()=>{
-    try{
-      await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-      app.on("error",(error)=>{
-        console.log("Err:",error)
-        throw error
-      })
-      app.listen(process.env.PORT,()=>{
-        console.log(`App is listening on port ${process.env.PORT}`)
-      })
-    }
-    catch(error){
-        console.log(" Error:",error);
-        throw error
-    }
-})()
+
+connectDB()
+
+dotenv.config({
+  path: './env'
+})
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app=express()
+
+// ;(async ()=>{
+//     try{
+//       await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//       app.on("error",(error)=>{
+//         console.log("Err:",error)
+//         throw error
+//       })
+//       app.listen(process.env.PORT,()=>{
+//         console.log(`App is listening on port ${process.env.PORT}`)
+//       })
+//     }
+//     catch(error){
+//         console.log(" Error:",error);
+//         throw error
+//     }
+// })()
