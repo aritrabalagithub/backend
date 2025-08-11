986f9e0fe3a75c3556cb70ba1bfd8681dae5c8f3
Aritra Bala <aritra07@gmail.com>
2025-08-11 15:15:32 +0530

    fix: corrected type declaration for avatar field in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -25,7 +25,7 @@ const userSchema=new Schema(
             index:true
         },
         avatar:{
-            type:string,
+            type:String,
             required:true,
             },
         coverImage:{
